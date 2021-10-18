080c5ad102e90110e1eb2dd427338fbf7289e78e
Nathan <1350592737@qq.com>
2021-10-18 15:34:46 +0800

    delete hrnet

--- hRnet/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import sys 
-BASE_DIR = "/media/ders/zhangyumin/ritm_interactive_segmentation/"
-sys.path.append(BASE_DIR)
-# import models 
-from  export import init_predictor
-from  hRnet.predictor import Hr_predictor
-from hRnet.baseclicker import Clicker,Click
